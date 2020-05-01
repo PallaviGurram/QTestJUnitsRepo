ddc515bdc8d577fc355188cebd6325fc6e710452
PallaviGurram <64623186+PallaviGurram@users.noreply.github.com>
2020-05-01 07:41:27 +0530

    Initial Project for jenkins testing

---
+++ qtest/src/test/java/com/test/qtest/qtest/HelloWorldTest.java
@@ -0,0 +1,41 @@
+package com.test.qtest.qtest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public HelloWorldTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( HelloWorldTest.class );
+    }
+
+    public void testGetHelloMessageTrue(){
+    	HelloWorld hello = new HelloWorld();
+    	assertEquals(hello.getHelloMessage(),"hello!");
+    	
+    }
+    public void testGetHelloMessageFalse(){
+    	HelloWorld hello = new HelloWorld();
+    	assertEquals(hello.getHelloMessage(),"hello!");
+    	
+    }
+}
